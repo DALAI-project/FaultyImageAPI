7df42d7f4d5c4b197aee0730feed38d26f419c49
fohra <atte.fohr@gmail.com>
2023-03-24 11:49:34 +0000

    added ability to use writing type classifier

--- api.py
+++ api.py
@@ -9,10 +9,12 @@ from torchvision.transforms.functional import InterpolationMode
 from pdf2image import convert_from_bytes
 from flask import Flask, request, Response
 import io
-import time
+from flask_cors import CORS
+
 
 # Creates the Flask app
 app = Flask(__name__)
+CORS(app)
 
 # Transform methods for corner & post-it model inputs
 data_transforms = transforms.Compose([
@@ -27,18 +29,25 @@ empty_transforms = transforms.Compose([
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
 
+writing_type_transforms = transforms.Compose([
+        transforms.Resize((224, 224), interpolation=InterpolationMode.BICUBIC),
+        transforms.ToTensor(),
+        transforms.Normalize([0.882, 0.883, 0.899], [0.088, 0.089, 0.094])
+    ])
 
 # Models are expected to be saved in ./mallit folder in the same path as the code
 MODEL_PATH = './mallit/' 
 POST_IT_MODEL = 'post_it_model_20122022.onnx'
 CORNER_MODEL = 'corner_model_19122022.onnx'
 EMPTY_MODEL = 'empty_model_v4.onnx'
+WRITING_TYPE_MODEL = 'writing_type_v1.onnx'
 
 try:
     # Load the models and the trained weights
     post_it_model = onnxruntime.InferenceSession(MODEL_PATH + POST_IT_MODEL)
     corner_model = onnxruntime.InferenceSession(MODEL_PATH + CORNER_MODEL)
     empty_model = onnxruntime.InferenceSession(MODEL_PATH + EMPTY_MODEL)
+    writing_type_model = onnxruntime.InferenceSession(MODEL_PATH + WRITING_TYPE_MODEL)
 except Exception as e:
     print("Failed to load pretrained models: {}".format(e))
 
@@ -51,7 +60,7 @@ def predict_fault(image, model):
     return preds.item()
 
 # Method for getting predictions based on the arguments given in the query string
-def predict_labels(images, args, name, extension, col_names):
+def predict_labels(images, args, name, extension, col_names,idj):
     result_list = []
     # Loop over images
     for i, image in enumerate(images):
@@ -84,7 +93,15 @@ def predict_labels(images, args, name, extension, col_names):
             # Transformations for empty page detection
             input_image = empty_transforms(image).unsqueeze(0)
             label = predict_fault(input_image, empty_model)
-            res_dict['tyhja_sivu'] = 1 - label        
+            res_dict['tyhja_sivu'] = 1 - label  
+
+        if args[3] == '1':
+            # Transformations for writing_type page detection
+            input_image = writing_type_transforms(image).unsqueeze(0)
+            label = predict_fault(input_image, writing_type_model)
+            res_dict['kirjoitustyyppi'] = label
+            
+        res_dict['id']= idj     
 
         # Result is a list of dicts, one for each processed page
         result_list.append(res_dict)
@@ -99,8 +116,10 @@ def detect():
     postit  = request.args.get('postit', None)
     corner  = request.args.get('corner', None)
     empty  = request.args.get('empty', None)
+    writing_type  = request.args.get('writing_type', None)
+    idj = request.args.get('id',None)
     # Save arguments in a list
-    args = [postit, corner, empty]
+    args = [postit, corner, empty, writing_type, idj]
 
     # Expects message body to have the key "image"
     if request.files.get("image"):
@@ -125,9 +144,10 @@ def detect():
             images = [image.convert("RGB")]
 
         # Column names for the .csv file
-        col_names = ['tiedosto','post_it','taittunut_kulma','tyhja_sivu']
+        col_names = ['tiedosto','post_it','taittunut_kulma','tyhja_sivu', 'kirjoitustyyppi', 'id']
         # Get prediction results for each image as a list of dicts
-        result_list = predict_labels(images, args, name, extension, col_names)
+        result_list = predict_labels(images, args, name, extension, col_names,idj)
+        return result_list
         result_df = pd.DataFrame.from_dict(result_list)
 
         end = time.time()
@@ -143,4 +163,6 @@ def detect():
         print("POST-request does not contain input image.")
 
 if __name__ == "__main__":
+    #app.run(port=, host='')
+
     app.run(port=5000)
